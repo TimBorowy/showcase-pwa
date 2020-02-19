c2d6659d41a4bf94ce28552921dcae998983fef6
Tim Borowy <blastertimbo@gmail.com>
2020-02-19 20:16:07 +0100

    add the rest of serviceworker

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import router from './router'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
+import './registerServiceWorker'
 
 Vue.use(VueMaterial)
 
